c7abfeb0538dadaec80ca501c626332658e721b1
NoobGrinder420 <d1h2a3n4v5i6n@gmail.com>
2025-02-22 00:21:27 +0800

    update

--- Dyslexhelp/build.sh
+++ Dyslexhelp/build.sh
@@ -10,6 +10,5 @@ source venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
 
-export GOOGLE_APPLICATION_CREDENTIALS="/Users/dhanvins_com/PythonComputing/Projects/dotted-task-451114-s6-dc663783f317.json" 
 python3 main.py
 deactivate # Deactivate virtual environment (after the app finishes)
