7fe454619fb8ec3fdcb759627c400d75a4194aee
Dhanvin <139748617+NoobGrinder420@users.noreply.github.com>
2025-03-05 21:53:26 +0800

    Update build.sh

--- PythonComputing/Projects/Dyslexhelp/build.sh
+++ PythonComputing/Projects/Dyslexhelp/build.sh
@@ -11,7 +11,7 @@ pip install --upgrade pip
 pip install -r requirements.txt
 brew install tesseract  # Or appropriate Tesseract installation for your OS
 
-# Set environment variable AND run the app in the SAME shell session
-export GOOGLE_APPLICATION_CREDENTIALS="/Users/dhanvins_com/PythonComputing/Projects/dotted-task-451114-s6-dc663783f317.json" python3 main.py
+# run the app
+python3 main.py
 
 deactivate # Deactivate virtual environment (after the app finishes)
